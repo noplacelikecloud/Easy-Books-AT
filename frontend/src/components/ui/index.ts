4fbad6f8f11189802d0aa9dfa4697d172fcdb72e
fluer-cancom <bernhard.fluer@cancom.com>
2026-09-09 14:56:11 +0200

    feat(ui): create Fluer UI components barrel export and verify build

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,42 @@
+// Fluer Design System UI Primitives for Easy-Books
+
+// Core
+export { Button } from "./core/Button";
+export type { ButtonProps } from "./core/Button";
+export { IconButton } from "./core/IconButton";
+export type { IconButtonProps } from "./core/IconButton";
+export { Badge } from "./core/Badge";
+export type { BadgeProps, BadgeTone } from "./core/Badge";
+export { Tag } from "./core/Tag";
+export type { TagProps } from "./core/Tag";
+export { Card } from "./core/Card";
+export type { CardProps } from "./core/Card";
+
+// Forms
+export { Input } from "./forms/Input";
+export type { InputProps } from "./forms/Input";
+export { Select } from "./forms/Select";
+export type { SelectProps, SelectOption } from "./forms/Select";
+export { Checkbox } from "./forms/Checkbox";
+export type { CheckboxProps } from "./forms/Checkbox";
+export { Switch } from "./forms/Switch";
+export type { SwitchProps } from "./forms/Switch";
+
+// Feedback & Navigation
+export { Dialog } from "./feedback/Dialog";
+export type { DialogProps } from "./feedback/Dialog";
+export { Toast } from "./feedback/Toast";
+export type { ToastProps, ToastTone } from "./feedback/Toast";
+export { Tabs } from "./navigation/Tabs";
+export type { TabsProps, TabItem } from "./navigation/Tabs";
+
+// Table System
+export {
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableHead,
+  TableCell,
+} from "./table/Table";
+export type { TableProps, TableCellProps } from "./table/Table";
